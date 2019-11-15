69806102e0307e504ffaced358b5557c40f97873
Jeremy Jung <jjchan921@gmail.com>
2019-11-14 19:07:57 -0500

    Initial Commit

---
+++ StudySession/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudySession
+//
+//  Created by Jeremy Jung on 11/14/19.
+//  Copyright © 2019 Jeremy Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
